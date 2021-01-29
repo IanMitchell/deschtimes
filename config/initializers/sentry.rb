f94830f748d3f05608b71272642b1b319d07579c
Ian Mitchell <ian.mitchell@hey.com>
2021-01-29 11:43:55 -0800

    Move Sentry into a Job

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,13 @@
 Sentry.init do |config|
   config.dsn = 'https://d893c9c36f594926a3c83233273a5b0a@o165375.ingest.sentry.io/5590539'
   config.breadcrumbs_logger = [:active_support_logger]
+
+  config.enabled_environments = %w[production]
+
+  config.async = lambda { |event|
+    SentryJob.perform_later(event)
+  }
+
   config.traces_sampler = lambda do |context|
     Rails.env.production?
   end
