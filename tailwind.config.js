7635ef27675b97940feae67315090db7f6b88e5d
Ian Mitchell <ian.mitchell@hey.com>
2021-01-29 08:47:12 -0800

    Purge Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: ['./app/**/*'],
   theme: {
     extend: {
       fontFamily: {
