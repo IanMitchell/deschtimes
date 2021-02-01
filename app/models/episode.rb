2bd7e1f23adca632b303de6ae93f0714ce658891
Ian Mitchell <ian.mitchell@hey.com>
2021-02-01 13:21:12 -0800

    Use Webhook Name

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -137,6 +137,7 @@ class Episode < ApplicationRecord
         end
 
         Discord::Notifier.message embed,
+                                  username: webhook.name,
                                   url: webhook.url,
                                   avatar_url: avatar
       end
--- app/models/staff.rb
+++ app/models/staff.rb
@@ -73,6 +73,7 @@ class Staff < ApplicationRecord
         end
 
         Discord::Notifier.message embed,
+                                  username: webhook.name,
                                   url: webhook.url,
                                   avatar_url: avatar
       end
